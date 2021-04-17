810ff39ea36fc0c460c7010807d630017d708586
senkentarou <masahiro-senda@c-fo.com>
2021-04-17 23:05:10 +0900

    update vimrc

--- .vim/tool/keybind_table.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for keytype in nnoremap inoremap; do
-    for prefix in C S; do
-        for letter in {a..z}; do
-            echo "${keytype} <${prefix}-${letter}> <${prefix}-${letter}>"
-        done
-    done
-done
