55cfbd156ea68e0284f243330877b9475539cb7a
senkentarou <masahiro-senda@c-fo.com>
2022-08-07 17:05:32 +0900

    refact vim options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,33 +1,23 @@
 --
 -- Vim options
 --
-vim.cmd([[
-  set guifont=Droid\ Sans\ Mono\ for\ Powerline\ Nerd\ Font\ Complete\ 12
-  set undofile
-  set cmdheight=2
-  set ignorecase
-  set hidden
-  set autoread
-  set noshowmode
-  " Colorscheme
-  set termguicolors
-  colorscheme hybrid
-  " File encoding
-  set encoding=utf-8
-  " Line settings
-  set number
-  set backspace=indent,eol,start
-  set whichwrap=b,s,<,>,[,],h,l
-  set cursorline
-  " Clipboard sharing
-  set clipboard=unnamed
-  " Completion settings
-  set completeopt=menuone,noinsert,noselect
-  set shortmess+=c
-  " Tab settings
-  set expandtab
-  set smarttab
-  set shiftwidth=2
-  set softtabstop=2
-  set tabstop=2
-]])
+vim.cmd([[ colorscheme hybrid ]])
+vim.opt.undofile = true
+vim.opt.cmdheight = 2
+vim.opt.ignorecase = true
+vim.opt.hidden = true
+vim.opt.autoread = true
+vim.opt.termguicolors = true
+vim.opt.encoding = 'utf-8'
+vim.opt.number = true
+vim.opt.backspace = [[indent,eol,start]]
+vim.opt.whichwrap = [[b,s,<,>,[,],h,l]]
+vim.opt.cursorline = true
+vim.opt.clipboard = 'unnamed'
+vim.opt.completeopt = [[menuone,noinsert,noselect]]
+vim.opt.shortmess = vim.opt.shortmess + { c = true }
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
