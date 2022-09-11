698d2828c7d43ac098e8a7af72298c76773d1e5a
senkentarou <masahiro-senda@c-fo.com>
2022-09-11 10:34:49 +0900

    add wrap option

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -11,6 +11,7 @@ vim.opt.termguicolors = true
 vim.opt.encoding = 'utf-8'
 vim.opt.number = true
 vim.opt.backspace = [[indent,eol,start]]
+vim.opt.wrap = true
 vim.opt.whichwrap = [[b,s,<,>,[,],h,l]]
 vim.opt.cursorline = true
 vim.opt.clipboard = 'unnamed'
