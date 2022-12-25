e67bb93d75abdbc5370d590aa9361e01fc608c02
senkentarou <masahiro-senda@c-fo.com>
2022-12-25 23:10:03 +0900

    setup mason-lspconfig

--- .config/nvim/lua/configs/completions.lua
+++ .config/nvim/lua/configs/completions.lua
@@ -13,6 +13,7 @@ require('fidget').setup {
 
 -- mason (lsp server installer)
 require('mason').setup {}
+require('mason-lspconfig').setup {}
 
 -- nvim-lspconfig: use diagnostics and formatting for ruby/rspec and nvim-compe nvim_lsp setting to auto import on typescriptreact
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
