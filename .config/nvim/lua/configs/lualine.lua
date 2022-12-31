9fb726e47182093032b49daed469cd6053760254
senkentarou <masahiro-senda@c-fo.com>
2022-12-31 11:41:21 +0900

    update lualine settings

--- .config/nvim/lua/configs/lualine.lua
+++ .config/nvim/lua/configs/lualine.lua
@@ -1,7 +1,23 @@
+local function LspClient()
+  local msg = 'No Active Lsp'
+  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
+    return msg
+  end
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      return client.name
+    end
+  end
+  return msg
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'base16',
+    theme = 'jellybeans',
     component_separators = {
       left = '',
       right = '',
@@ -29,11 +45,47 @@ require('lualine').setup {
     },
     lualine_b = {
       'branch',
-      'diff',
-      'diagnostics',
+      {
+        'diff',
+        symbols = {
+          added = ' ',
+          modified = ' ',
+          removed = ' ',
+        },
+      },
+      {
+        'diagnostics',
+        sources = {
+          'nvim_diagnostic',
+        },
+        sections = {
+          'error',
+          'warn',
+          'info',
+          'hint',
+        },
+      },
     },
     lualine_c = {
-      'filename',
+      {
+        'filename',
+        file_status = true, -- Displays file status (readonly status, modified status)
+        newfile_status = false, -- Display new file status (new file means no write after created)
+        path = 1,
+        -- 0: Just the filename
+        -- 1: Relative path
+        -- 2: Absolute path
+        -- 3: Absolute path, with tilde as the home directory
+        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+        -- for other components. (terrible name, any suggestions?)
+        symbols = {
+          modified = '[+]', -- Text to show when the file is modified.
+          readonly = '[RO]', -- Text to show when the file is non-modifiable or readonly.
+          unnamed = '[No Name]', -- Text to show for unnamed buffers.
+          newfile = '[New]', -- Text to show for new created file before first writting
+        },
+      },
+      LspClient,
     },
     lualine_x = {
       'encoding',
