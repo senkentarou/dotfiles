35c7f349660f8d1fd85fc9717219299881cad7c5
senkentarou <masahiro-senda@c-fo.com>
2023-01-10 19:38:57 +0900

    disable format before save

--- .config/nvim/lua/configs/completions.lua
+++ .config/nvim/lua/configs/completions.lua
@@ -199,24 +199,6 @@ null_ls.setup {
     null_ls.builtins.formatting.jq,
     require('typescript.extensions.null-ls.code-actions'),
   },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({
-        group = augroup,
-        buffer = bufnr,
-      })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            bufnr = bufnr,
-            async = true,
-          })
-        end,
-      })
-    end
-  end,
 }
 
 -- null-ls typescript integration
