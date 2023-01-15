c5b76e8294e599da562115808b68d1c355dc1d6d
senkentarou <masahiro-senda@c-fo.com>
2023-01-15 17:23:24 +0900

    introduce incremental_selection
    press <CR> on normal mode!

--- .config/nvim/lua/configs/completions.lua
+++ .config/nvim/lua/configs/completions.lua
@@ -414,6 +414,15 @@ require('nvim-treesitter.configs').setup {
     'markdown',
     'bash',
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      node_incremental = "<CR>",
+      scope_incremental = "<S-CR>",
+      node_decremental = "<BS>",
+    },
+  },
   highlight = {
     enable = true,
   },
