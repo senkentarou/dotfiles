f41bff27ad4e787b5e31f0a002bad847ab87360f
senkentarou <masahiro-senda@c-fo.com>
2023-01-15 18:02:31 +0900

    delete unuse setting

--- .config/nvim/lua/configs/auto_commands.lua
+++ .config/nvim/lua/configs/auto_commands.lua
@@ -1,18 +1,6 @@
 --
 -- Auto commands
 --
-function _G.set_terminal_keymaps()
-  local opts = {
-    buffer = 0,
-  }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-end
-
 vim.cmd([[
   " Disable auto complete on comment next line
   autocmd FileType * setlocal formatoptions-=ro
