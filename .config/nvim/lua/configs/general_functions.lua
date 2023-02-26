89a0d2673c04952bb7db8bed740df63fc2e3aa1a
senkentarou <masahiro-senda@c-fo.com>
2023-02-26 09:31:32 +0900

    disable mru section

--- .config/nvim/lua/configs/general_functions.lua
+++ .config/nvim/lua/configs/general_functions.lua
@@ -23,6 +23,7 @@ vim.cmd([[
 local alpha = require('alpha')
 local dashboard = require('alpha.themes.startify')
 dashboard.section.header.val = {} -- clear header art
+dashboard.section.mru.val = {}
 alpha.setup(dashboard.config)
 
 require('indent_blankline').setup {
