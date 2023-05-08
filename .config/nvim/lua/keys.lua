2d95881a73ba35cccc2d12d453123efe18c52342
senkentarou <masahiro-senda@c-fo.com>
2023-05-08 23:54:48 +0900

    switch live grep and grep string

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -63,12 +63,12 @@ vim.cmd([[
   nnoremap <silent> <Leader>p :<C-u>Gocd<CR>
   nnoremap <silent> <Leader>P :<C-u>Gopr<CR>
   " grep words
-  nnoremap <silent> <Leader>n :<C-u>lua require('telescope.builtin').live_grep({ default_text = vim.fn.histget('@', -1) })<CR>
-  nnoremap <silent> <Leader>N :<C-u>lua require('telescope.builtin').grep_string({ search = vim.fn.histget('@', -1) })<CR>
-  nnoremap <silent> <Leader>, :<C-u>lua require('telescope.builtin').live_grep({ default_text = require('global_functions').cword_as_input() })<CR>
-  nnoremap <silent> <Leader>< :<C-u>lua require('telescope.builtin').grep_string({ search = require('global_functions').cword_as_input() })<CR>
-  nnoremap <silent> <Leader>m :<C-u>lua require('telescope.builtin').live_grep({ default_text = vim.fn.input('[LiveGrep] ') })<CR>
-  nnoremap <silent> <Leader>M :<C-u>lua require('telescope.builtin').grep_string({ search = vim.fn.input('[GrepString] ') })<CR>
+  nnoremap <silent> <Leader>n :<C-u>lua require('telescope.builtin').grep_string({ search = vim.fn.histget('@', -1) })<CR>
+  nnoremap <silent> <Leader>N :<C-u>lua require('telescope.builtin').live_grep({ default_text = vim.fn.histget('@', -1) })<CR>
+  nnoremap <silent> <Leader>, :<C-u>lua require('telescope.builtin').grep_string({ search = require('global_functions').cword_as_input() })<CR>
+  nnoremap <silent> <Leader>< :<C-u>lua require('telescope.builtin').live_grep({ default_text = require('global_functions').cword_as_input() })<CR>
+  nnoremap <silent> <Leader>m :<C-u>lua require('telescope.builtin').grep_string({ search = vim.fn.input('[GrepString] ') })<CR>
+  nnoremap <silent> <Leader>M :<C-u>lua require('telescope.builtin').live_grep({ default_text = vim.fn.input('[LiveGrep] ') })<CR>
   " buffers
   nnoremap <Leader>w :<C-u>w<CR>
   nnoremap <silent> <Leader>W :lua vim.lsp.buf.format({ async = true })<CR>
