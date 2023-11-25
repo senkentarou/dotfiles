ebd17262c0f6c196c53d55468ff2fefab47381f7
senkentarou <masahiro-senda@c-fo.com>
2023-11-25 09:58:39 +0900

    update bash binds

--- .bash.d/binds.sh
+++ .bash.d/binds.sh
@@ -2,10 +2,18 @@
 # bind
 #
 
-fzf_history() {
+fzf_select_file() {
+	local file
+	file=$(fzf --select-1 --reverse --height 30% | xargs -I{} echo vim {})
+	READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}${file}${READLINE_LINE:$READLINE_POINT}"
+	READLINE_POINT=$((READLINE_POINT + ${#file}))
+}
+bind -x '"\C-f": fzf_select_file'
+
+fzf_select_history() {
 	local hist
 	hist=$(HISTTIMEFORMAT='' history | gsed -e 's/^[[:space:]]*[0-9]\+[[:space:]]*//' | sort -u | fzf --select-1 --reverse --height 30% --query "$READLINE_LINE")
 	READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}${hist}${READLINE_LINE:$READLINE_POINT}"
 	READLINE_POINT=$((READLINE_POINT + ${#hist}))
 }
-bind -x '"\C-r": fzf_history'
+bind -x '"\C-r": fzf_select_history'
