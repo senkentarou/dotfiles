a70cb74ef3b2871d5f2d6072c323eff48e8eefbb
senkentarou <masahiro-senda@c-fo.com>
2023-11-25 10:28:37 +0900

    add gsed condition

--- .bash.d/binds.sh
+++ .bash.d/binds.sh
@@ -11,8 +11,15 @@ fzf_select_file() {
 bind -x '"\C-f": fzf_select_file'
 
 fzf_select_history() {
+	local sed
+	if [ -n "$(command -v gsed 2>/dev/null)" ]; then
+		sed='gsed'
+	else
+		sed='sed'
+	fi
+
 	local hist
-	hist=$(HISTTIMEFORMAT='' history | gsed -e 's/^[[:space:]]*[0-9]\+[[:space:]]*//' | sort -u | fzf --select-1 --reverse --height 30% --query "$READLINE_LINE")
+	hist=$(HISTTIMEFORMAT='' history | $sed -e 's/^[[:space:]]*[0-9]\+[[:space:]]*//' | sort -u | fzf --select-1 --reverse --height 30% --query "$READLINE_LINE")
 	READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}${hist}${READLINE_LINE:$READLINE_POINT}"
 	READLINE_POINT=$((READLINE_POINT + ${#hist}))
 }
