fe2fbc3ada2a26933554e8f7c8e68e0b6494cd7e
senkentarou <masahiro-senda@c-fo.com>
2024-12-16 10:07:35 +0900

    fix shellcheck

--- .bash.d/exports.sh
+++ .bash.d/exports.sh
@@ -7,8 +7,7 @@ export LSCOLORS=gxfxcxdxbxegedabagacad
 
 # history settings
 export HISTCONTROL=erasedups
-#shellcheck disable=SC2125
-export HISTIGNORE=history:exit:?:?? # ignore 1,2 chars commands
+export HISTIGNORE='history:exit:?:??' # ignore 1,2 chars commands
 
 export PROMPT_COMMAND="history -a;${PROMPT_COMMAND}"
 shopt -u histappend
